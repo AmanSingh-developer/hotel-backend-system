63ab3f437f7ada8d1318203f167cf30ba5466884
AmanSingh-developer <amansngh1511@gmail.com>
2026-03-10 16:51:55 +0530

    shift mingodb to online server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require('dotenv').config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
   res.send("welcome to my Hotel");
@@ -19,6 +21,8 @@ const menuItem = require("./models/menuItem");
 // use the router
 app.use("/person", personRoutes);
 app.use("/menu", menuItemRoutes);
+
+
 
 app.listen(3000, () => {
   console.log("server is started");
